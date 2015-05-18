13e1d7f4dd2970d0a43915463e7c45ec121f5a81
Karrex <e.chernihovsky@gmail.com>
2015-05-18 16:10:35 +0300

    Add Comparable

--- src/Library.java
+++ src/Library.java
@@ -1,8 +1,12 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Library {
+    private static final Logger logger = Logger.getLogger(Library.class.getName());
+
     public static void readBooks(File f) {
         ArrayList<Book> books = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(f))) {
@@ -10,11 +14,12 @@ public class Library {
             while ((s = br.readLine()) != null) {
                 String[] book = s.split(";");
                 books.add(new Book(book[0], book[1], Integer.parseInt(book[2])));
+                logger.log(Level.INFO, "read books");
             }
             Collections.sort(books);
             System.out.println(books);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "exception in readBooks()", e);
         }
     }
 
@@ -24,11 +29,13 @@ public class Library {
         String autor = book.getAutor();
         int year = book.getYear();
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
+            System.out.println();
             bw.write(title + ";" + autor + ";" + year + ";");
             bw.newLine();
             bw.flush();
+            logger.log(Level.INFO, "write books");
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "exception in writeBook()", e);
         }
     }
 }
